30114ace2d0cfe7b79a8ec82fc9824e9366d1ede
Lenny Caraballo <lcaraballo@gmail.com>
2018-03-01 10:06:14 -0500

    Adding hash and equals to serviceStatus RouteDetail

--- mta-otp-proxy/src/main/java/com/camsys/shims/service_status/model/RouteDetail.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/service_status/model/RouteDetail.java
@@ -110,5 +110,36 @@ public class RouteDetail {
         this.lastUpdated = lastUpdated;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        RouteDetail that = (RouteDetail) o;
+
+        if (routeName != null ? !routeName.equals(that.routeName) : that.routeName != null) return false;
+        if (color != null ? !color.equals(that.color) : that.color != null) return false;
+        if (mode != null ? !mode.equals(that.mode) : that.mode != null) return false;
+        if (agency != null ? !agency.equals(that.agency) : that.agency != null) return false;
+        if (routeId != null ? !routeId.equals(that.routeId) : that.routeId != null) return false;
+        if (routeSortOrder != null ? !routeSortOrder.equals(that.routeSortOrder) : that.routeSortOrder != null)
+            return false;
+        if (inService != null ? !inService.equals(that.inService) : that.inService != null) return false;
+        if (lastUpdated != null ? !lastUpdated.equals(that.lastUpdated) : that.lastUpdated != null) return false;
+        return statusDetailsList != null ? statusDetailsList.equals(that.statusDetailsList) : that.statusDetailsList == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = routeName != null ? routeName.hashCode() : 0;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        result = 31 * result + (mode != null ? mode.hashCode() : 0);
+        result = 31 * result + (agency != null ? agency.hashCode() : 0);
+        result = 31 * result + (routeId != null ? routeId.hashCode() : 0);
+        result = 31 * result + (routeSortOrder != null ? routeSortOrder.hashCode() : 0);
+        result = 31 * result + (inService != null ? inService.hashCode() : 0);
+        result = 31 * result + (lastUpdated != null ? lastUpdated.hashCode() : 0);
+        result = 31 * result + (statusDetailsList != null ? statusDetailsList.hashCode() : 0);
+        return result;
+    }
 }
