f810b52cdea6668956bcf839d8124544d07093f4
mmaranda-cs <mmaranda@camsys.com>
2018-04-02 17:55:01 -0400

    MOTP-649 replaces the waiting on block with something like the copy on write pattern. I am concerned about memory ramifications from this.

--- mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
@@ -23,6 +23,9 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
     public void setGtfsPath(String path) { _gtfsPath = path; }
     public String getGtfsPath() { return _gtfsPath; }
 
+    GtfsRelationalDaoImpl _prime;
+    GtfsRelationalDaoImpl _secondary;
+
     public void load(){
         _isLoading = true;
 
@@ -30,8 +33,20 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
 
         _log.info("Is loading a DAO for path {} with file size {} at time {}", _gtfsPath, file.length(), DateTime.now().toLocalTime().toString("HH:mm:ss"));
 
+        loadDao(_prime, file);
+
+        _log.info("Is done loading a DAO for path {} at time {}", _gtfsPath, DateTime.now().toLocalTime().toString("HH:mm:ss"));
+
+        _isLoading = false;
+
+        loadDao(_secondary, file);
+
+    }
+
+    private void loadDao(GtfsRelationalDaoImpl daoToReload, File file)
+    {
         GtfsReader reader = new GtfsReader();
-        reader.setEntityStore(this);
+        reader.setEntityStore(daoToReload);
         try {
             reader.setInputLocation(file);
             reader.run();
@@ -39,115 +54,134 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
         } catch (IOException e) {
             throw new RuntimeException("Failure while reading GTFS", e);
         }
-
-        _log.info("Is done loading a DAO for path {} at time {}", _gtfsPath, DateTime.now().toLocalTime().toString("HH:mm:ss"));
-
-        _isLoading = false;
     }
 
-    /**
-     * This method blocks until the bundle is ready--this method is called as part of the proxy to each of the underlying
-     * methods of the TDS and NYC-TDS below to ensure all calls to those bundle-backed methods succeed (i.e. the bundle is ready
-     * to be queried.)
-     */
-    private void blockUntilBundleIsReady() {
-        try {
-            while(_isLoading) {
-                _blockedRequestCounter++;
-
-                // only print this every 25 times so we don't fill up the logs!
-                if(_blockedRequestCounter > 25) {
-                    _log.warn("Dao is not ready we've blocked 25 requests for this dao since last log event.");
-                    _blockedRequestCounter = 0;
-                }
-
-                synchronized(this) {
-                    Thread.sleep(250);
-                    Thread.yield();
-                }
-            }
-        } catch(InterruptedException e) {
-            return;
-        }
-    }
 
     public List<String> getTripAgencyIdsReferencingServiceId(AgencyAndId serviceId) {
-        blockUntilBundleIsReady();
-        return super.getTripAgencyIdsReferencingServiceId(serviceId);
+        if(_isLoading)
+        {
+            return _secondary.getTripAgencyIdsReferencingServiceId(serviceId);
+        }
+        return _prime.getTripAgencyIdsReferencingServiceId(serviceId);
     }
 
     public List<Route> getRoutesForAgency(Agency agency) {
-        blockUntilBundleIsReady();
-        return super.getRoutesForAgency(agency);
+        if(_isLoading)
+        {
+            return _secondary.getRoutesForAgency(agency);
+        }
+        return _prime.getRoutesForAgency(agency);
     }
 
     public List<Stop> getStopsForStation(Stop station) {
-        blockUntilBundleIsReady();
-        return super.getStopsForStation(station);
+        if(_isLoading)
+        {
+            return _secondary.getStopsForStation(station);
+        }
+        return _prime.getStopsForStation(station);
     }
 
     public List<AgencyAndId> getAllShapeIds() {
-        blockUntilBundleIsReady();
-        return super.getAllShapeIds();
+        if(_isLoading)
+        {
+            return _secondary.getAllShapeIds();
+        }
+        return _prime.getAllShapeIds();
     }
 
     public List<ShapePoint> getShapePointsForShapeId(AgencyAndId shapeId) {
-        blockUntilBundleIsReady();
-        return super.getShapePointsForShapeId(shapeId);
+        if(_isLoading)
+        {
+            return _secondary.getShapePointsForShapeId(shapeId);
+        }
+        return _prime.getShapePointsForShapeId(shapeId);
     }
 
     public List<StopTime> getStopTimesForTrip(Trip trip) {
-        blockUntilBundleIsReady();
-        return super.getStopTimesForTrip(trip);
+        if(_isLoading)
+        {
+            return _secondary.getStopTimesForTrip(trip);
+        }
+        return _prime.getStopTimesForTrip(trip);
     }
 
     public List<StopTime> getStopTimesForStop(Stop stop) {
-        blockUntilBundleIsReady();
-        return super.getStopTimesForStop(stop);
+        if(_isLoading)
+        {
+            return _secondary.getStopTimesForStop(stop);
+        }
+        return _prime.getStopTimesForStop(stop);
     }
 
     public List<Trip> getTripsForRoute(Route route) {
-        blockUntilBundleIsReady();
-        return super.getTripsForRoute(route);
+        if(_isLoading)
+        {
+            return _secondary.getTripsForRoute(route);
+        }
+        return _prime.getTripsForRoute(route);
     }
 
     public List<Trip> getTripsForShapeId(AgencyAndId shapeId) {
-        blockUntilBundleIsReady();
-        return super.getTripsForShapeId(shapeId);
+        if(_isLoading)
+        {
+            return _secondary.getTripsForShapeId(shapeId);
+        }
+        return _prime.getTripsForShapeId(shapeId);
     }
 
     public List<Trip> getTripsForServiceId(AgencyAndId serviceId) {
-        blockUntilBundleIsReady();
-        return super.getTripsForServiceId(serviceId);
+        if(_isLoading)
+        {
+            return _secondary.getTripsForServiceId(serviceId);
+        }
+        return _prime.getTripsForServiceId(serviceId);
     }
 
     public List<Trip> getTripsForBlockId(AgencyAndId blockId) {
-        blockUntilBundleIsReady();
-        return super.getTripsForBlockId(blockId);
+        if(_isLoading)
+        {
+            return _secondary.getTripsForBlockId(blockId);
+        }
+        return _prime.getTripsForBlockId(blockId);
     }
 
     public List<Frequency> getFrequenciesForTrip(Trip trip) {
-        blockUntilBundleIsReady();
-        return super.getFrequenciesForTrip(trip);
+        if(_isLoading)
+        {
+            return _secondary.getFrequenciesForTrip(trip);
+        }
+        return _prime.getFrequenciesForTrip(trip);
     }
 
     public List<AgencyAndId> getAllServiceIds() {
-        blockUntilBundleIsReady();
-        return super.getAllServiceIds();
+        if(_isLoading)
+        {
+            return _secondary.getAllServiceIds();
+        }
+        return _prime.getAllServiceIds();
     }
 
     public List<ServiceCalendarDate> getCalendarDatesForServiceId(AgencyAndId serviceId) {
-        blockUntilBundleIsReady();
-        return super.getCalendarDatesForServiceId(serviceId);
+        if(_isLoading)
+        {
+            return _secondary.getCalendarDatesForServiceId(serviceId);
+        }
+        return _prime.getCalendarDatesForServiceId(serviceId);
     }
 
     public ServiceCalendar getCalendarForServiceId(AgencyAndId serviceId) {
-        blockUntilBundleIsReady();
-        return super.getCalendarForServiceId(serviceId);
+        if(_isLoading)
+        {
+            return _secondary.getCalendarForServiceId(serviceId);
+        }
+        return _prime.getCalendarForServiceId(serviceId);
     }
 
     public List<FareRule> getFareRulesForFareAttribute(FareAttribute fareAttribute) {
-        blockUntilBundleIsReady();
-        return super.getFareRulesForFareAttribute(fareAttribute);
+        if(_isLoading)
+        {
+            return _secondary.getFareRulesForFareAttribute(fareAttribute);
+        }
+        return _prime.getFareRulesForFareAttribute(fareAttribute);
     }
 }
