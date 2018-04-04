10576e978d533022a1d01197ec52e486eb4b2214
mmaranda-cs <mmaranda@camsys.com>
2018-04-04 11:47:24 -0400

    MOTP-649 Fixes the updateableGtfsRelactionalDao to use a more concise reload and replace pattern.

--- mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
@@ -16,172 +16,109 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
 
     private String _gtfsPath;
 
-    private int _blockedRequestCounter = 0;
-
-    private boolean _isLoading = false;
-
     public void setGtfsPath(String path) { _gtfsPath = path; }
     public String getGtfsPath() { return _gtfsPath; }
 
-    GtfsRelationalDaoImpl _prime = new GtfsRelationalDaoImpl();
-    GtfsRelationalDaoImpl _secondary = new GtfsRelationalDaoImpl();;
+    GtfsRelationalDaoImpl _dao = new GtfsRelationalDaoImpl();
+    public GtfsRelationalDaoImpl getRelaodedDao(){
+        return _dao;
+    }
+    public void setReloadedDao(GtfsRelationalDaoImpl dao)
+    {
+        _dao = dao;
+    }
+
 
     public void load(){
-        _isLoading = true;
-
         File file = new File(_gtfsPath);
 
         _log.info("Is loading a DAO for path {} with file size {} at time {}", _gtfsPath, file.length(), DateTime.now().toLocalTime().toString("HH:mm:ss"));
 
-        loadDao(_prime, file);
+        loadDao(file);
 
         _log.info("Is done loading a DAO for path {} at time {}", _gtfsPath, DateTime.now().toLocalTime().toString("HH:mm:ss"));
-
-        _isLoading = false;
-
-        loadDao(_secondary, file);
-
     }
 
-    private void loadDao(GtfsRelationalDaoImpl daoToReload, File file)
+    private void loadDao(File file)
     {
+        GtfsRelationalDaoImpl dao = new GtfsRelationalDaoImpl();
         GtfsReader reader = new GtfsReader();
-        reader.setEntityStore(daoToReload);
+
+        reader.setEntityStore(dao);
+
         try {
             reader.setInputLocation(file);
             reader.run();
             reader.close();
+
+            setReloadedDao(dao);
         } catch (IOException e) {
             throw new RuntimeException("Failure while reading GTFS", e);
         }
     }
 
-
     public List<String> getTripAgencyIdsReferencingServiceId(AgencyAndId serviceId) {
-        if(_isLoading)
-        {
-            return _secondary.getTripAgencyIdsReferencingServiceId(serviceId);
-        }
-        return _prime.getTripAgencyIdsReferencingServiceId(serviceId);
+        return getRelaodedDao().getTripAgencyIdsReferencingServiceId(serviceId);
     }
 
     public List<Route> getRoutesForAgency(Agency agency) {
-        if(_isLoading)
-        {
-            return _secondary.getRoutesForAgency(agency);
-        }
-        return _prime.getRoutesForAgency(agency);
+        return getRelaodedDao().getRoutesForAgency(agency);
     }
 
     public List<Stop> getStopsForStation(Stop station) {
-        if(_isLoading)
-        {
-            return _secondary.getStopsForStation(station);
-        }
-        return _prime.getStopsForStation(station);
+        return getRelaodedDao().getStopsForStation(station);
     }
 
     public List<AgencyAndId> getAllShapeIds() {
-        if(_isLoading)
-        {
-            return _secondary.getAllShapeIds();
-        }
-        return _prime.getAllShapeIds();
+        return getRelaodedDao().getAllShapeIds();
     }
 
     public List<ShapePoint> getShapePointsForShapeId(AgencyAndId shapeId) {
-        if(_isLoading)
-        {
-            return _secondary.getShapePointsForShapeId(shapeId);
-        }
-        return _prime.getShapePointsForShapeId(shapeId);
+        return getRelaodedDao().getShapePointsForShapeId(shapeId);
     }
 
     public List<StopTime> getStopTimesForTrip(Trip trip) {
-        if(_isLoading)
-        {
-            return _secondary.getStopTimesForTrip(trip);
-        }
-        return _prime.getStopTimesForTrip(trip);
+        return getRelaodedDao().getStopTimesForTrip(trip);
     }
 
     public List<StopTime> getStopTimesForStop(Stop stop) {
-        if(_isLoading)
-        {
-            return _secondary.getStopTimesForStop(stop);
-        }
-        return _prime.getStopTimesForStop(stop);
+        return getRelaodedDao().getStopTimesForStop(stop);
     }
 
     public List<Trip> getTripsForRoute(Route route) {
-        if(_isLoading)
-        {
-            return _secondary.getTripsForRoute(route);
-        }
-        return _prime.getTripsForRoute(route);
+        return getRelaodedDao().getTripsForRoute(route);
     }
 
     public List<Trip> getTripsForShapeId(AgencyAndId shapeId) {
-        if(_isLoading)
-        {
-            return _secondary.getTripsForShapeId(shapeId);
-        }
-        return _prime.getTripsForShapeId(shapeId);
+        return getRelaodedDao().getTripsForShapeId(shapeId);
     }
 
     public List<Trip> getTripsForServiceId(AgencyAndId serviceId) {
-        if(_isLoading)
-        {
-            return _secondary.getTripsForServiceId(serviceId);
-        }
-        return _prime.getTripsForServiceId(serviceId);
+        return getRelaodedDao().getTripsForServiceId(serviceId);
     }
 
     public List<Trip> getTripsForBlockId(AgencyAndId blockId) {
-        if(_isLoading)
-        {
-            return _secondary.getTripsForBlockId(blockId);
-        }
-        return _prime.getTripsForBlockId(blockId);
+        return getRelaodedDao().getTripsForBlockId(blockId);
     }
 
     public List<Frequency> getFrequenciesForTrip(Trip trip) {
-        if(_isLoading)
-        {
-            return _secondary.getFrequenciesForTrip(trip);
-        }
-        return _prime.getFrequenciesForTrip(trip);
+        return getRelaodedDao().getFrequenciesForTrip(trip);
     }
 
     public List<AgencyAndId> getAllServiceIds() {
-        if(_isLoading)
-        {
-            return _secondary.getAllServiceIds();
-        }
-        return _prime.getAllServiceIds();
+        return getRelaodedDao().getAllServiceIds();
     }
 
     public List<ServiceCalendarDate> getCalendarDatesForServiceId(AgencyAndId serviceId) {
-        if(_isLoading)
-        {
-            return _secondary.getCalendarDatesForServiceId(serviceId);
-        }
-        return _prime.getCalendarDatesForServiceId(serviceId);
+        return getRelaodedDao().getCalendarDatesForServiceId(serviceId);
     }
 
     public ServiceCalendar getCalendarForServiceId(AgencyAndId serviceId) {
-        if(_isLoading)
-        {
-            return _secondary.getCalendarForServiceId(serviceId);
-        }
-        return _prime.getCalendarForServiceId(serviceId);
+        return getRelaodedDao().getCalendarForServiceId(serviceId);
     }
 
     public List<FareRule> getFareRulesForFareAttribute(FareAttribute fareAttribute) {
-        if(_isLoading)
-        {
-            return _secondary.getFareRulesForFareAttribute(fareAttribute);
-        }
-        return _prime.getFareRulesForFareAttribute(fareAttribute);
+        return getRelaodedDao().getFareRulesForFareAttribute(fareAttribute);
     }
+
 }
