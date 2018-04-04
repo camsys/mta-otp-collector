c7b1c6a3fd90d703b66eec18d9a01d2644e24f47
mmaranda-cs <mmaranda@camsys.com>
2018-04-04 14:44:45 -0400

    MOTP-649 makes sure all implemented calls rerout to the dao that is reloaded in place. This is unfortunately brittle if the underlying implementations gain new methods.

--- mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/factory/UpdateableGtfsRelationalDao.java
@@ -1,7 +1,9 @@
 package com.camsys.shims.factory;
 
 import org.joda.time.DateTime;
+import org.onebusaway.gtfs.impl.GenericDaoImpl;
 import org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl;
+import org.onebusaway.gtfs.impl.MultipleCalendarsForServiceIdException;
 import org.onebusaway.gtfs.model.*;
 import org.onebusaway.gtfs.serialization.GtfsReader;
 import org.slf4j.Logger;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.*;
 
 public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
@@ -20,14 +23,15 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
     public String getGtfsPath() { return _gtfsPath; }
 
     GtfsRelationalDaoImpl _dao = new GtfsRelationalDaoImpl();
+
     public GtfsRelationalDaoImpl getRelaodedDao(){
         return _dao;
     }
+
     public void setReloadedDao(GtfsRelationalDaoImpl dao)
     {
         _dao = dao;
     }
-
 
     public void load(){
         File file = new File(_gtfsPath);
@@ -121,4 +125,201 @@ public class UpdateableGtfsRelationalDao extends GtfsRelationalDaoImpl {
         return getRelaodedDao().getFareRulesForFareAttribute(fareAttribute);
     }
 
+    public void clearAllCaches() {
+        getRelaodedDao().clearAllCaches();
+    }
+
+    public boolean isPackStopTimes() {
+        return getRelaodedDao().isPackStopTimes();
+    }
+
+    public void setPackStopTimes(boolean packStopTimes) {
+        getRelaodedDao().setPackStopTimes(packStopTimes);
+    }
+
+    public boolean isPackShapePoints() {
+        return getRelaodedDao().isPackShapePoints();
+    }
+
+    public void setPackShapePoints(boolean packShapePoints) {
+        getRelaodedDao().setPackShapePoints(packShapePoints);
+    }
+
+    public Agency getAgencyForId(String id) {
+        return (Agency)getRelaodedDao().getEntityForId(Agency.class, id);
+    }
+
+    public Collection<Agency> getAllAgencies() {
+        return getRelaodedDao().getAllEntitiesForType(Agency.class);
+    }
+
+    public Collection<Block> getAllBlocks() {
+        return getRelaodedDao().getAllEntitiesForType(Block.class);
+    }
+
+    public Collection<ServiceCalendarDate> getAllCalendarDates() {
+        return getRelaodedDao().getAllEntitiesForType(ServiceCalendarDate.class);
+    }
+
+    public Collection<ServiceCalendar> getAllCalendars() {
+        return getRelaodedDao().getAllEntitiesForType(ServiceCalendar.class);
+    }
+
+    public Collection<FareAttribute> getAllFareAttributes() {
+        return getRelaodedDao().getAllEntitiesForType(FareAttribute.class);
+    }
+
+    public Collection<FareRule> getAllFareRules() {
+        return getRelaodedDao().getAllEntitiesForType(FareRule.class);
+    }
+
+    public Collection<FeedInfo> getAllFeedInfos() {
+        return getRelaodedDao().getAllEntitiesForType(FeedInfo.class);
+    }
+
+    public Collection<Frequency> getAllFrequencies() {
+        return getRelaodedDao().getAllEntitiesForType(Frequency.class);
+    }
+
+    public Collection<Route> getAllRoutes() {
+        return getRelaodedDao().getAllEntitiesForType(Route.class);
+    }
+
+    public Collection<ShapePoint> getAllShapePoints() {
+        return getRelaodedDao().getAllShapePoints();
+    }
+
+    public Collection<StopTime> getAllStopTimes() {
+        return getRelaodedDao().getAllStopTimes();
+    }
+
+    public Collection<Stop> getAllStops() {
+        return getRelaodedDao().getAllStops();
+    }
+
+    public Collection<Transfer> getAllTransfers() {
+        return getRelaodedDao().getAllTransfers();
+    }
+
+    public Collection<Trip> getAllTrips() {
+        return getRelaodedDao().getAllTrips();
+    }
+
+    public Block getBlockForId(int id) {
+        return getRelaodedDao().getBlockForId(id);
+    }
+
+    public ServiceCalendarDate getCalendarDateForId(int id) {
+        return getRelaodedDao().getCalendarDateForId(id);
+    }
+
+    public ServiceCalendar getCalendarForId(int id) {
+        return getRelaodedDao().getCalendarForId(id);
+    }
+
+    public FareAttribute getFareAttributeForId(AgencyAndId id) {
+        return getRelaodedDao().getFareAttributeForId(id);
+    }
+
+    public FareRule getFareRuleForId(int id) {
+        return getRelaodedDao().getFareRuleForId(id);
+    }
+
+    public FeedInfo getFeedInfoForId(String id) {
+        return (FeedInfo)this.getEntityForId(FeedInfo.class, id);
+    }
+
+    public Frequency getFrequencyForId(int id) {
+        return (Frequency)this.getEntityForId(Frequency.class, id);
+    }
+
+    public Collection<Pathway> getAllPathways() {
+        return this.getAllEntitiesForType(Pathway.class);
+    }
+
+    public Pathway getPathwayForId(AgencyAndId id) {
+        return (Pathway)this.getEntityForId(Pathway.class, id);
+    }
+
+    public Route getRouteForId(AgencyAndId id) {
+        return (Route)this.getEntityForId(Route.class, id);
+    }
+
+    public ShapePoint getShapePointForId(int id) {
+        return getRelaodedDao().getShapePointForId(id);
+    }
+
+    public Stop getStopForId(AgencyAndId id) {
+        return getRelaodedDao().getStopForId(id);
+    }
+
+    public StopTime getStopTimeForId(int id) {
+        return getRelaodedDao().getStopTimeForId(id);
+    }
+
+    public Transfer getTransferForId(int id) {
+        return getRelaodedDao().getTransferForId(id);
+    }
+
+    public Trip getTripForId(AgencyAndId id) {
+        return getRelaodedDao().getTripForId(id);
+    }
+
+    public <K, V> Map<K, V> getEntitiesByIdForEntityType(Class<K> keyType, Class<V> entityType) {
+        return getRelaodedDao().getEntitiesByIdForEntityType(keyType, entityType);
+    }
+
+    public <T> Collection<T> getAllEntitiesForType(Class<T> type) {
+        return getRelaodedDao().getAllEntitiesForType(type);
+    }
+
+    public <T> T getEntityForId(Class<T> type, Serializable id) {
+        return getRelaodedDao().getEntityForId(type, id);
+    }
+
+    public void saveEntity(Object entity) {
+        getRelaodedDao().saveEntity(entity);
+    }
+
+    public <T> void clearAllEntitiesForType(Class<T> type) {
+        getRelaodedDao().clearAllEntitiesForType(type);
+    }
+
+    public <K extends Serializable, T extends IdentityBean<K>> void removeEntity(T entity) {
+        getRelaodedDao().removeEntity(entity);
+    }
+
+    public void close() {
+        getRelaodedDao().close();
+    }
+
+    public void setGenerateIds(boolean generateIds) {
+        getRelaodedDao().setGenerateIds(generateIds);
+    }
+
+    public Set<Class<?>> getEntityClasses() {
+        return getRelaodedDao().getEntityClasses();
+    }
+
+    public void clear() {
+        getRelaodedDao().clear();
+    }
+
+    public void updateEntity(Object entity) {
+    }
+
+    public void saveOrUpdateEntity(Object entity) {
+        IdentityBean<Serializable> bean = (IdentityBean)entity;
+        Object existing = this.getEntityForId(entity.getClass(), bean.getId());
+        if (existing != entity) {
+            this.saveEntity(entity);
+        }
+    }
+
+
+    public void open() {
+    }
+
+    public void flush() {
+    }
 }
