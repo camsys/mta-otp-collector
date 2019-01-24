9bf71fce145b7df442a1736ee4b5a4964ab195c2
sheldonabrown <sheldonb@gmail.com>
2019-01-24 07:47:43 -0500

    cleanup after debugging

--- mta-otp-proxy/src/main/java/com/camsys/shims/util/gtfs/RouteNameGtfsMap.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/util/gtfs/RouteNameGtfsMap.java
@@ -4,19 +4,14 @@ import com.camsys.shims.atis.csv.AtisGtfsEntry;
 import com.camsys.shims.util.gtfs.csv.RouteNameGtfsEntry;
 import org.onebusaway.csv_entities.CsvEntityReader;
 import org.onebusaway.gtfs.model.AgencyAndId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RouteNameGtfsMap {
-
-    private static final Logger _log = LoggerFactory.getLogger(RouteNameGtfsMap.class);
 
     private Map<String, AgencyAndId> _routeNameGtfsMap;
 
@@ -45,15 +40,7 @@ public class RouteNameGtfsMap {
     public AgencyAndId getId(String key) {
         if (key != null ) key = key.toUpperCase();
         AgencyAndId atisId = AgencyAndId.convertFromString(key);
-        AgencyAndId result = _routeNameGtfsMap.get(atisId.getId());
-        if (result == null) {
-            _log.info("atisId " + atisId.getId() + " not found in " + _routeNameGtfsMap.toString());
-        }
-        return result;
-    }
-
-    public Map<String, AgencyAndId> getUnmodifiableMap() {
-        return Collections.unmodifiableMap(_routeNameGtfsMap);
+        return _routeNameGtfsMap.get(atisId.getId());
     }
 
 }
