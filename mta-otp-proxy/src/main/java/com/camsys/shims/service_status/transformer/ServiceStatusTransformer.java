ba14bab765a8ea3cf40ddf8e68f6b8b9d210c5b2
Simon Jacobs <sidaja@gmail.com>
2019-02-20 14:19:52 -0500

    Remove GtfsAndCalendar reference

--- mta-otp-proxy/src/main/java/com/camsys/shims/service_status/transformer/ServiceStatusTransformer.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/service_status/transformer/ServiceStatusTransformer.java
@@ -2,10 +2,7 @@ package com.camsys.shims.service_status.transformer;
 
 import com.camsys.shims.service_status.adapters.GtfsRouteAdapter;
 import com.camsys.shims.service_status.model.RouteDetail;
-import com.camsys.shims.util.gtfs.GtfsAndCalendar;
-import org.onebusaway.gtfs.model.calendar.CalendarServiceData;
 import org.onebusaway.gtfs.services.GtfsDataService;
-import org.onebusaway.gtfs.services.GtfsRelationalDao;
 
 import java.util.List;
 import java.util.Map;
