8b7d025282dc1795efbfe5753c110fed6e4fbf55
sheldonabrown <sheldonb@gmail.com>
2020-03-19 18:12:32 -0400

    MOTP-1616 remove all HTML from SIRI output

--- mta-otp-proxy/src/main/java/com/camsys/shims/servlet/HttpRequestSiriSink.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/servlet/HttpRequestSiriSink.java
@@ -1,10 +1,15 @@
 package com.camsys.shims.servlet;
 
+import com.camsys.shims.util.HtmlCleanupUtil;
 import com.camsys.shims.util.source.MergingSiriSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.HttpRequestHandler;
+import uk.org.siri.siri.DefaultedTextStructure;
+import uk.org.siri.siri.PtSituationElementStructure;
+import uk.org.siri.siri.ServiceDelivery;
 import uk.org.siri.siri.Siri;
+import uk.org.siri.siri.SituationExchangeDeliveryStructure;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +35,11 @@ public class HttpRequestSiriSink implements HttpRequestHandler {
     private MergingSiriSource _siriSource;
     private JAXBContext _context = null;
     private String _type = GMS_TYPE;
+    private HtmlCleanupUtil _htmlCleanupUtil;
+
+    public void setHtmlCleanupUtil(HtmlCleanupUtil htmlCleanupUtil) {
+        _htmlCleanupUtil = htmlCleanupUtil;
+    }
 
     public void setSource(MergingSiriSource siriSource) {
         _siriSource = siriSource;
@@ -48,10 +58,40 @@ public class HttpRequestSiriSink implements HttpRequestHandler {
         }
 
         try {
-            httpServletResponse.getWriter().print(getXml(_siriSource.getFeed(), CIS_TYPE.equals(_type)));
+            httpServletResponse.getWriter().print(getXml(filterSiri(_siriSource.getFeed()), CIS_TYPE.equals(_type)));
         } catch (Exception e) {
             throw new IOException(e);
         }
+    }
+
+    public Siri filterSiri(Siri siri) {
+        if (siri.getServiceDelivery() != null) {
+            ServiceDelivery sd = siri.getServiceDelivery();
+            if (sd.getSituationExchangeDelivery() != null) {
+                for (SituationExchangeDeliveryStructure seds : sd.getSituationExchangeDelivery()) {
+                    if (seds.getSituations() != null) {
+                        SituationExchangeDeliveryStructure.Situations s = seds.getSituations();
+                        if (s.getPtSituationElement() != null) {
+                            for (PtSituationElementStructure pt : s.getPtSituationElement()) {
+                                if (pt.getSummary() != null) {
+                                    DefaultedTextStructure txt = pt.getSummary();
+                                    String html = txt.getValue();
+                                    String cleanedHtml = _htmlCleanupUtil.filterHtml(html);
+                                    txt.setValue(cleanedHtml);
+                                }
+                                if (pt.getDescription() != null) {
+                                    DefaultedTextStructure txt = pt.getDescription();
+                                    String html = txt.getValue();
+                                    String cleanedHtml = _htmlCleanupUtil.filterHtml(html);
+                                    txt.setValue(cleanedHtml);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return siri;
     }
 
     public String getXml(Siri siri, boolean isBuscis) throws Exception {
