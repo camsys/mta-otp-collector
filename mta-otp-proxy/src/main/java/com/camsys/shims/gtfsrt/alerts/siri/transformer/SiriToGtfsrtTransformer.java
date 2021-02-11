ae1c61ed7867a65c95d718c1f389bd400bc12d84
Jeff Maki <jmaki@camsys.com>
2021-02-11 13:03:07 -0500

    Update SiriToGtfsrtTransformer.java

--- mta-otp-proxy/src/main/java/com/camsys/shims/gtfsrt/alerts/siri/transformer/SiriToGtfsrtTransformer.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/gtfsrt/alerts/siri/transformer/SiriToGtfsrtTransformer.java
@@ -15,20 +15,28 @@ package com.camsys.shims.gtfsrt.alerts.siri.transformer;
 import com.camsys.shims.service_status.adapters.GtfsRouteAdapter;
 import com.camsys.shims.util.transformer.GtfsRealtimeTransformer;
 import com.google.transit.realtime.GtfsRealtime.Alert;
+import com.google.transit.realtime.GtfsRealtime.EntitySelector;
 import com.google.transit.realtime.GtfsRealtime.FeedEntity;
 import com.google.transit.realtime.GtfsRealtime.FeedHeader;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
 import com.google.transit.realtime.GtfsRealtime.TimeRange;
+import com.google.transit.realtime.GtfsRealtime.TranslatedString;
+import com.google.transit.realtime.GtfsRealtime.TripDescriptor;
 import com.google.transit.realtime.GtfsRealtimeConstants;
 import com.google.transit.realtime.GtfsRealtimeServiceStatus;
 import com.google.transit.realtime.GtfsRealtimeServiceStatus.MercuryAlert;
 import com.google.transit.realtime.GtfsRealtimeServiceStatus.MercuryAlert.Builder;
 
+import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.nyc.gtfsrt.util.GtfsRealtimeLibrary;
 import org.onebusaway.nyc.transit_data_manager.model.ExtendedServiceAlertBean;
 import org.onebusaway.nyc.transit_data_manager.util.NycSiriUtil;
+import org.onebusaway.transit_data.model.service_alerts.NaturalLanguageStringBean;
 import org.onebusaway.transit_data.model.service_alerts.ServiceAlertBean;
 import org.onebusaway.transit_data.model.service_alerts.SituationAffectsBean;
+import org.onebusaway.transit_data.model.service_alerts.SituationConsequenceBean;
+import org.onebusaway.transit_data.model.service_alerts.TimeRangeBean;
+
 import uk.org.siri.siri.Siri;
 
 import java.util.Date;
@@ -54,11 +62,11 @@ public class SiriToGtfsrtTransformer implements GtfsRealtimeTransformer<Siri> {
         header.setGtfsRealtimeVersion(GtfsRealtimeConstants.VERSION);
         message.setHeader(header);
 
-        for (ServiceAlertBean serviceAlert : serviceAlerts) {
+        for (ExtendedServiceAlertBean serviceAlert : serviceAlerts) {
             if (_gtfsRouteAdapter != null)
                 replaceRouteIds(serviceAlert);
             FeedEntity.Builder fe = FeedEntity.newBuilder();
-            Alert.Builder alert = GtfsRealtimeLibrary.makeAlert(serviceAlert);
+            Alert.Builder alert = makeAlert(serviceAlert);
 
             if(serviceAlert.getReason() != null) {
             	GtfsRealtimeServiceStatus.MercuryAlert.Builder mercuryBuilder = 
@@ -66,10 +74,11 @@ public class SiriToGtfsrtTransformer implements GtfsRealtimeTransformer<Siri> {
 
             	mercuryBuilder.setCreatedAt(serviceAlert.getCreationTime());
             	mercuryBuilder.setUpdatedAt(serviceAlert.getCreationTime());
+            	mercuryBuilder.setDisplayBeforeActive(0);
             	mercuryBuilder.setAlertType(serviceAlert.getReason());
-
+            	
             	alert.setExtension(GtfsRealtimeServiceStatus.mercuryAlert, mercuryBuilder.build());
-        	}
+            }
         
             if (_autoExpirySec > 0) {
                 for (TimeRange.Builder timeRange : alert.getActivePeriodBuilderList()) {
@@ -104,5 +113,108 @@ public class SiriToGtfsrtTransformer implements GtfsRealtimeTransformer<Siri> {
 
     public void setAutoExpirySec(int autoExpirySec) {
         _autoExpirySec = autoExpirySec;
+    }
+    
+    
+    
+    
+    /***
+     * This is all pulled from GtfsRealtimeLibrary in OBA NYC
+     */
+    
+    private static Alert.Builder makeAlert(ExtendedServiceAlertBean alert) {
+
+        Alert.Builder rtAlert = Alert.newBuilder();
+
+        if (alert.getPublicationWindows() != null) {
+            for (TimeRangeBean bean : alert.getPublicationWindows()) {
+                rtAlert.addActivePeriod(range(bean));
+            }
+        }
+
+        if (alert.getAllAffects() != null) {
+            for (SituationAffectsBean affects : alert.getAllAffects()) {
+                rtAlert.addInformedEntity(informedEntity(affects, alert));
+            }
+        }
+
+        if (alert.getConsequences() != null && !alert.getConsequences().isEmpty()) {
+            SituationConsequenceBean cb = alert.getConsequences().get(0);
+            // Effect and EEffect perfectly match string values
+            rtAlert.setEffect(Alert.Effect.valueOf(cb.getEffect().toString()));
+        }
+
+        if (alert.getUrls() != null) {
+            rtAlert.setUrl(translatedString(alert.getUrls()));
+        }
+
+        if (alert.getSummaries() != null) {
+            rtAlert.setHeaderText(translatedString(alert.getSummaries()));
+        }
+
+        if (alert.getDescriptions() != null) {
+            rtAlert.setDescriptionText(translatedString(alert.getDescriptions()));
+        }
+
+        return rtAlert;
+    }
+    
+    private static TimeRange.Builder range(TimeRangeBean range) {
+        TimeRange.Builder builder = TimeRange.newBuilder();
+        if (range.getFrom() > 0)
+            builder.setStart(range.getFrom()/1000);
+        if (range.getTo() > 0)
+            builder.setEnd(range.getTo()/1000);
+        return builder;
+    }
+
+    private static EntitySelector.Builder informedEntity(SituationAffectsBean bean, ExtendedServiceAlertBean alert) {
+        EntitySelector.Builder builder = EntitySelector.newBuilder();
+
+        // If there is a trip ID or a direction ID, use a TripDescriptor (no duplicate route info)
+        if (bean.getTripId() != null || bean.getDirectionId() != null) {
+            TripDescriptor.Builder td = TripDescriptor.newBuilder();
+            if (bean.getTripId() != null)
+                td.setTripId(id(bean.getTripId()));
+            if (bean.getRouteId() != null)
+                td.setRouteId(id(bean.getRouteId()));
+            if (bean.getDirectionId() != null)
+                td.setDirectionId(Integer.parseInt(bean.getDirectionId()));
+            builder.setTrip(td);
+        } else if (bean.getRouteId() != null) {
+            builder.setRouteId(id(bean.getRouteId()));
+        }
+
+        if (bean.getRouteId() != null)
+            builder.setAgencyId(AgencyAndId.convertFromString(bean.getRouteId()).getAgencyId());
+        if (bean.getAgencyId() != null)
+            builder.setAgencyId(bean.getAgencyId());
+        if (bean.getStopId() != null)
+            builder.setStopId(id(bean.getStopId()));
+
+    	GtfsRealtimeServiceStatus.MercuryEntitySelector.Builder mercurySelectorBuilder = 
+        		GtfsRealtimeServiceStatus.MercuryEntitySelector.newBuilder();
+
+    	mercurySelectorBuilder.setSortOrder(builder.getAgencyId() + ":" + 
+    			builder.getRouteId() + ":" + alert.getMessagePriority());
+    	
+        builder.setExtension(GtfsRealtimeServiceStatus.mercuryEntitySelector, mercurySelectorBuilder.build());
+                
+        return builder;
+    }
+    
+    private static TranslatedString.Builder translatedString(List<NaturalLanguageStringBean> beans) {
+        TranslatedString.Builder string = TranslatedString.newBuilder();
+        for (NaturalLanguageStringBean bean : beans) {
+            TranslatedString.Translation.Builder tr = TranslatedString.Translation.newBuilder();
+            tr.setLanguage(bean.getLang());
+            tr.setText(bean.getValue());
+            string.addTranslation(tr);
+        }
+        return string;
+    }
+
+    private static String id(String agencyAndId) {
+        return AgencyAndId.convertFromString(agencyAndId).getId();
     }
 }
