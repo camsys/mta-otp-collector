f7a14c5ebe0695aaa274edfd54d8f208ce41ec14
sheldonabrown <sheldonb@gmail.com>
2023-02-18 10:19:12 -0500

    adding header support to Deserializer

--- mta-otp-proxy/src/main/java/com/camsys/shims/gtfsrt/tripUpdates/lirr/deserializer/LIRRFeedMessageDeserializer.java
+++ mta-otp-proxy/src/main/java/com/camsys/shims/gtfsrt/tripUpdates/lirr/deserializer/LIRRFeedMessageDeserializer.java
@@ -20,9 +20,18 @@ import com.google.transit.realtime.GtfsRealtimeNYCT;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class LIRRFeedMessageDeserializer implements Deserializer<GtfsRealtime.FeedMessage> {
     private static final ExtensionRegistry _extensionRegistry;
+    private Map<String, String> _headers;
+    public void setApiHeaders(Map<String, String> headers) {
+        _headers = headers;
+    }
+    @Override
+    public Map<String, String> getApiHeaders() {
+        return _headers;
+    }
 
     static {
         _extensionRegistry = ExtensionRegistry.newInstance();
